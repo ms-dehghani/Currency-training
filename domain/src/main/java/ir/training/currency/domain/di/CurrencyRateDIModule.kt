7fdd6e99f903656697378e5c56db47089b9ddac9
sadegh1371 <sadegh.dehghani1992@gmail.com>
2024-02-16 21:03:42 +0330

    Add currency log DI

--- domain/src/main/java/ir/training/currency/domain/di/CurrencyRateDIModule.kt
+++ domain/src/main/java/ir/training/currency/domain/di/CurrencyRateDIModule.kt
@@ -5,7 +5,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import ir.training.currency.domain.repository.currency.item.CurrencyRateRepository
+import ir.training.currency.domain.repository.currency.log.CurrencyLogRepository
 import ir.training.currency.domain.usecase.currency.item.CurrencyRateUseCase
+import ir.training.currency.domain.usecase.currency.log.add.CurrencyLogAddUseCase
+import ir.training.currency.domain.usecase.currency.log.list.CurrencyLogListUseCase
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -13,5 +16,15 @@ class CurrencyRateDIModule {
     @Provides
     fun provideCurrencyRateUseCase(repository: CurrencyRateRepository): CurrencyRateUseCase {
         return CurrencyRateUseCase(repository)
+    }
+
+    @Provides
+    fun provideCurrencyLogListUseCase(repository: CurrencyLogRepository): CurrencyLogListUseCase {
+        return CurrencyLogListUseCase(repository)
+    }
+
+    @Provides
+    fun provideCurrencyLogAddUseCase(repository: CurrencyLogRepository): CurrencyLogAddUseCase {
+        return CurrencyLogAddUseCase(repository)
     }
 }
