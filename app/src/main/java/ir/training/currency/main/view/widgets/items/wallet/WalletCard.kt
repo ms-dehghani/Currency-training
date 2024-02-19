87d77e622d25beb373e071419f8db0b356ec1af1
sadegh1371 <sadegh.dehghani1992@gmail.com>
2024-02-19 21:40:01 +0330

    Refactor

--- app/src/main/java/ir/training/currency/main/view/widgets/items/wallet/WalletCard.kt
+++ app/src/main/java/ir/training/currency/main/view/widgets/items/wallet/WalletCard.kt
@@ -1,7 +1,6 @@
 package ir.training.currency.main.view.widgets.items.wallet
 
 
-import androidx.annotation.Px
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +14,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
@@ -23,8 +21,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ir.training.currency.R
 import ir.training.currency.main.theme.WalletGradientCenter
