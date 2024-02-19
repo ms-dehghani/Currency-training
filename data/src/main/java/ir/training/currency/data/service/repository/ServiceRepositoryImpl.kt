b9807268023515abda864357947221a8f6fc3e6f
sadegh1371 <sadegh.dehghani1992@gmail.com>
2024-02-19 21:01:09 +0330

    Add negative wallet check

--- data/src/main/java/ir/training/currency/data/service/repository/ServiceRepositoryImpl.kt
+++ data/src/main/java/ir/training/currency/data/service/repository/ServiceRepositoryImpl.kt
@@ -55,6 +55,16 @@ class ServiceRepositoryImpl @Inject constructor(val api: ApiService) :
                 val newCurrencyAmount = roundNumber(amount * it, 2)
                 val currencyExchangeCommission = roundNumber(amount * commission, 2)
 
+                if((wallet.getCurrencyList().find { it.name == from }?.amount
+                        ?: 0.0) < (amount + currencyExchangeCommission)
+                ){
+                    return ExchangeItem(
+                        response = "You don't have enough ${from}.",
+                        walletItem = wallet,
+                        logList = getCurrencyLogList()
+                    )
+                }
+
                 wallet.addCurrency(WalletCurrency(to, newCurrencyAmount))
                 wallet.addCurrency(WalletCurrency(from, -roundNumber((amount + currencyExchangeCommission),2)))
 
